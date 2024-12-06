1229dceee599da1671f3cb44cf39693aa21fdc6b
Hansen <hansen.zheng@hotmail.com>
2024-12-06 09:36:38 +0800

    change cc image to inline

--- components/CreativeCommons.tsx
+++ components/CreativeCommons.tsx
@@ -2,13 +2,13 @@ import Image from 'next/image'
 
 export default function CreativeCommonsLicense() {
   return (
-    <p>
+    <p className="flex items-center gap-1">
       This work is licensed under{' '}
       <a
         href="https://creativecommons.org/licenses/by-nc/4.0/?ref=chooser-v1"
         target="_blank"
         rel="license noopener noreferrer"
-        style={{ display: 'inline-block' }}
+        className="inline-flex items-center gap-1"
       >
         Creative Commons Attribution-NonCommercial 4.0 International
         <Image
@@ -16,21 +16,21 @@ export default function CreativeCommonsLicense() {
           alt=""
           width={22}
           height={22}
-          style={{ marginLeft: '3px', verticalAlign: 'text-bottom' }}
+          className="inline-block"
         />
         <Image
           src="https://mirrors.creativecommons.org/presskit/icons/by.svg"
           alt=""
           width={22}
           height={22}
-          style={{ marginLeft: '3px', verticalAlign: 'text-bottom' }}
+          className="inline-block"
         />
         <Image
           src="https://mirrors.creativecommons.org/presskit/icons/nc.svg"
           alt=""
           width={22}
           height={22}
-          style={{ marginLeft: '3px', verticalAlign: 'text-bottom' }}
+          className="inline-block"
         />
       </a>
     </p>
