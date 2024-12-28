29ba1f4cae1a1f96138d2c0499dff482e203ad34
Hansen <hansen.zheng@hotmail.com>
2024-12-28 16:34:03 +0800

    update header css style

--- components/Header.tsx
+++ components/Header.tsx
@@ -29,7 +29,7 @@ const Header = () => {
         </div>
       </Link>
       <div className="flex items-center space-x-4 leading-5 sm:space-x-6">
-        <div className="no-scrollbar hidden max-w-40 items-center space-x-4 overflow-x-auto sm:flex sm:space-x-6 md:max-w-72 lg:max-w-full">
+        <div className="no-scrollbar hidden max-w-full items-center space-x-4 overflow-x-auto sm:flex sm:space-x-6 md:max-w-full lg:max-w-full">
           {headerNavLinks
             .filter((link) => link.href !== '/')
             .map((link) => (
