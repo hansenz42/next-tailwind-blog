4222eae78777f72420b2a90ac53811dc54aa634e
Hansen <hansen.zheng@hotmail.com>
2025-06-18 09:00:13 +0800

    add json sampler project

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -13,7 +13,7 @@ const projectsData: Project[] = [
   //   href: 'https://suspect.assen.top',
   // },
   {
-    title: '个人知识库',
+    title: '阿森知识库 ｜ Obsidian 双链笔记知识库',
     description:
       '使用 Obsidian 双链笔记知识库，涵盖计算机科学、数学、机器学习、软件工程、文史哲的个人笔记。',
     imgSrc: '/static/images/project-garden.png',
@@ -25,6 +25,12 @@ const projectsData: Project[] = [
     imgSrc: '/static/images/project-mindgraph.png',
     href: 'https://mindgraph.assen.top',
   },
+  {
+    title: 'JSON Sampler | JSON 简短示例生成工具',
+    description: '写技术文档时，是否因为 JSON 列表过长而困扰？JSON Sampler 可对 JSON 中的列表采样，生成一个简短的 JSON 示例。',
+    imgSrc: '/static/images/project-sampler.png',
+    href: 'https://sampler.assen.top',
+  }
 ]
 
 export default projectsData
