ae711fcd0c2015828a224a769b84f9ca9674ae73
Hansen <hansen.zheng@hotmail.com>
2025-12-24 19:41:40 +0800

    add Miremo project details and image to projectsData

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,17 +1,17 @@
 interface Project {
-  title: string,
-  description: string,
-  href?: string,
-  imgSrc?: string,
+  title: string
+  description: string
+  href?: string
+  imgSrc?: string
 }
 
 const projectsData: Project[] = [
-  // {
-  //   title: '正转裁判 —— 模仿逆转裁判的 AI 对话小游戏',
-  //   description: `就像《逆转裁判》那样，AI 扮演证人，而你作为侦探，要找到证人言词中的矛盾，并用证据指出对方的矛盾，找到事情的真相`,
-  //   imgSrc: '/static/images/project-lawyer.jpg',
-  //   href: 'https://suspect.assen.top',
-  // },
+  {
+    title: 'Miremo 米墨笔记 ｜ AI 知识图谱笔记工具',
+    description: '轻松记录，AI 整理，知识图谱索引，高效管理知识',
+    imgSrc: '/static/images/miremo.png',
+    href: 'https://www.miremoapp.com',
+  },
   {
     title: '阿森知识库 ｜ Obsidian 双链笔记知识库',
     description:
@@ -27,10 +27,11 @@ const projectsData: Project[] = [
   },
   {
     title: 'JSON Sampler | JSON 简短示例生成工具',
-    description: '写技术文档时，是否因为 JSON 列表过长而困扰？JSON Sampler 可对 JSON 中的列表采样，生成一个简短的 JSON 示例。',
+    description:
+      '写技术文档时，是否因为 JSON 列表过长而困扰？JSON Sampler 可对 JSON 中的列表采样，生成一个简短的 JSON 示例。',
     imgSrc: '/static/images/project-sampler.png',
     href: 'https://sampler.assen.top',
-  }
+  },
 ]
 
 export default projectsData
