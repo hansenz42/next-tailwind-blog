f7c7db69818f739d37d06d4de597ede3d59bdf4a
Hansen <hansen.zheng@hotmail.com>
2025-12-31 14:26:51 +0800

    fix: update greeting text for clarity and add English translation; adjust tag data format

--- app/Main.tsx
+++ app/Main.tsx
@@ -21,21 +21,24 @@ export default function Home({ posts }) {
           <div className="mx-auto max-w-3xl space-y-4 text-center xl:col-span-4">
             <div className="text-9xl">👋</div>
             <h1 className="text-3xl leading-9 font-extrabold tracking-tight text-gray-900 sm:text-4xl sm:leading-10 dark:text-gray-100">
-              &nbsp;&nbsp;&nbsp;你好，我是{mainContent.name} (Hello, I am {mainContent.name})
+              I am {mainContent.name}
             </h1>
             <h2 className="prose text-lg text-gray-600 dark:text-gray-400">
               {siteMetadata.description}
             </h2>
             <h2 className="prose text-lg text-gray-600 dark:text-gray-400">
-              写作即思考，另有更多的笔记存放在
+              写作即思考，另有
               <span className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400">
                 <Link href="https://garden.assen.top">🗄️知识库</Link>
               </span>
-              ，开卷有益。 (Writing is thinking, and there are more notes stored in
+              ，开卷有益。
+            </h2>
+            <h2 className="prose text-lg text-gray-600 dark:text-gray-400">
+              Writing is thinking, and check more notes in &nbsp;
               <span className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400">
                 <Link href="https://garden.assen.top">🗄️Knowledge Base</Link>
               </span>
-              , reading is beneficial.)
+              &nbsp;, reading is beneficial.
             </h2>
           </div>
         </div>
